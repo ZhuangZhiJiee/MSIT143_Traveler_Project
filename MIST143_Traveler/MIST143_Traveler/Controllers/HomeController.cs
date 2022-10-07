98240a48307df9408799011f06d5a63d26ce8d8b
AlanLeafR <ran315431@gmail.com>
2022-10-07 17:00:31 +0800

    1111

--- MIST143_Traveler/MIST143_Traveler/Controllers/HomeController.cs
+++ MIST143_Traveler/MIST143_Traveler/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace MIST143_Traveler.Controllers
 
         public IActionResult Index()
         {
-            return View();//branch test//45454545 
+            return View();//branch test//測試
         }
 
         public IActionResult Privacy()
