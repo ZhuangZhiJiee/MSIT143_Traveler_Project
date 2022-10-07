1bbf89b321f18ef55725bec27c6e7209a5ffd99c
ZhuangZhiJiee <chaosabyss73@gmail.com>
2022-10-07 18:01:12 +0800

    100$

--- MIST143_Traveler/MIST143_Traveler/Controllers/HomeController.cs
+++ MIST143_Traveler/MIST143_Traveler/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace MIST143_Traveler.Controllers
 
         public IActionResult Index()
         {
-            return View();//branch test//蒜泥白肉好好吃100元便當店
+            return View();//branch test//蒜泥白肉好好吃200元便當店
 
         }
 
