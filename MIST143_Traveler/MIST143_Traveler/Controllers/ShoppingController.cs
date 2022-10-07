2245f250dabb545877658dfcb38323127aee8657
ZhuangZhiJiee <chaosabyss73@gmail.com>
2022-10-07 19:40:08 +0800

    高歌離席123

--- MIST143_Traveler/MIST143_Traveler/Controllers/ShoppingController.cs
+++ MIST143_Traveler/MIST143_Traveler/Controllers/ShoppingController.cs
@@ -10,7 +10,7 @@ namespace MIST143_Traveler.Controllers
     {
         public IActionResult PayData()
         {
-            //高歌離席
+            //高歌離席123
             return View();
         }
         public IActionResult PayCheckout()
