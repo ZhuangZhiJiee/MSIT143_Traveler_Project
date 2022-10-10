165d4af026c2ca729e13688380c6a6cd71ae6dc0
莊智傑 <chaosabyss73@gmail.com>
2022-10-10 21:52:59 +0800

    結帳完成-智傑

--- MIST143_Traveler/MIST143_Traveler/wwwroot/js/travelOrder.js
+++ MIST143_Traveler/MIST143_Traveler/wwwroot/js/travelOrder.js
@@ -4,7 +4,8 @@ $(function () {
         let currentNum = parseInt($('#adultNum').text())
         currentNum += 1
         $('#adultNum').text(currentNum)
-        
+
+        $('#totalPrice').text(currentNum * 300)
     });
     $('#adultMinus').click(function () {
         let currentNum = parseInt($('#adultNum').text())
@@ -12,12 +13,16 @@ $(function () {
             currentNum -= 1
             $('#adultNum').text(currentNum)
         }
+        $('#totalPrice').text(currentNum * 300)
         console.log($('#adultNum').text())
     });
     $('#childPlus').click(function () {
         let currentNum = parseInt($('#childNum').text())
         currentNum += 1
         $('#childNum').text(currentNum)
+        let total = currentNum * 300
+        $('#shoppingCartTotalPrice').text(total)
+        $('#totalPayPrice').text(total)
         
     });
     $('#childMinus').click(function () {
@@ -26,6 +31,27 @@ $(function () {
             currentNum -= 1
             $('#childNum').text(currentNum)
         }
+        let total = currentNum * 300
+        $('#shoppingCartTotalPrice').text(total)
+        $('#totalPayPrice').text(total)
     });
     // $('.goPayBtn').
+
+    // 頁面跳轉
+    $('#modalCheck').click(function () {
+        $(location).attr('href', "/");
+    });
+
+    //信用卡動畫 - 點到出現 點到其他付款方式 則消失
+    $('#creditCardRadio').click(function () {
+        $('#creditCardBlock').show()
+    });
+    $('#flexRadioDefault1').click(function () {
+        $('#creditCardBlock').hide()
+    });
+    $('#flexRadioDefault2').click(function () {
+        $('#creditCardBlock').hide()
+    });
+
+    //creditCardBlock
 });
